d8c106e086e2bcf289c7517c4eb03db1e821cb0c
stevegocoding <stevegocoding@gmail.com>
2015-10-15 00:07:41 -0400

    create project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to url-shortener-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/url_shortener_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns url-shortener-clj.core-test
+  (:require [clojure.test :refer :all]
+            [url-shortener-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
